3eb3c0520af9eaa2a14dcb2085b5fee0acce2316
PiotrBerebecki <petebere@gmail.com>
2016-09-25 22:33:44 +0100

    Refactor test helpers

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,4 @@
-import _$ from 'jquery';
+import jqeury from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
@@ -9,15 +9,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+// Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = jqeury(window);
 
-chaiJquery(chai, chai.util, $);
-
+// Build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
@@ -26,11 +26,15 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
+// Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
 };
+
+// Set up chai-jquery
+chaiJquery(chai, chai.util, $);
 
 export {renderComponent, expect};
